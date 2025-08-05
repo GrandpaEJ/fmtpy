015f85cddd0022f693a7e21ea0cc1bba24ca16eb
Grandpa EJ <103351465+GrandpaEJ@users.noreply.github.com>
2025-08-05 22:45:12 +0600

    note: fmt2 > fmtpy

--- example/main.go
+++ example/main.go
@@ -1,18 +1,18 @@
 package main
 
-import "fmt2"
+import "fmtpy"
 
 func main() {
 	// Test the Input function
-	name := fmt2.Input("What's your name? ")
+	name := fmtpy.Input("What's your name? ")
 	
 	// Test different Print styles
-	fmt2.Print("Hello %s!", name)                    // Go style
-	fmt2.Print(`f"Nice to meet you, {name}"`, name) // Python style
-	fmt2.Print(42)                                  // Simple print number
-	fmt2.Print("Here's a string")                   // Simple print string
+	fmtpy.Print("Hello %s!", name)                    // Go style
+	fmtpy.Print(`f"Nice to meet you, {name}"`, name) // Python style
+	fmtpy.Print(42)                                  // Simple print number
+	fmtpy.Print("Here's a string")                   // Simple print string
 	
 	// Get more input with newline
-	hobby := fmt2.Input("What's your favorite hobby?\n")
-	fmt2.Print(`f"So you like {hobby}, that's cool!"`, hobby)
+	hobby := fmtpy.Input("What's your favorite hobby?\n")
+	fmtpy.Print(`f"So you like {hobby}, that's cool!"`, hobby)
 }
